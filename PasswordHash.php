67d99180613c497f663234f1a0c5f1f349583d98
Gael Abadin <gael.abadin@gmail.com>
2014-06-28 13:42:59 +0200

    Add json, add .gitignore, clean up doc files

--- PasswordHash.php
+++ PasswordHash.php
@@ -1,34 +1,35 @@
 <?php
-#
-# Portable PHP password hashing framework.
-#
-# Version 0.3 / Synapp (by elcodedocle, 2014).
-#
-# Original written by Solar Designer <solar at openwall.com> in 2004-2006 and placed in
-# the public domain.  Revised in subsequent years, still public domain.
-#
-# There's absolutely no warranty.
-#
-# The homepage URL for the original framework is:
-#
-#    http://www.openwall.com/phpass/
-#
-# Please be sure to update the Version line if you edit this file in any way.
-# It is suggested that you leave the main version number intact, but indicate
-# your project name (after the slash) and add your own revision information.
-#
-# Please do not change the "private" password hashing method implemented in
-# here, thereby making your hashes incompatible.  However, if you must, please
-# change the hash type identifier (the "$P$") to something different.
-#
-# Obviously, since this code is in the public domain, the above are not
-# requirements (there can be none), but merely suggestions.
-#
-# elcodedocle's rev: I've ignored Solar Designer's recommendation but maintained full
-# compatibility. Compatibility with future phpass versions is not warranted, though.
-# 
-# Latest version and revision notes available at https://github.com/elcodedocle/phpass
-#
+/** 
+ * Portable PHP password hashing framework.
+ *
+ * Version 0.3 / SynappV2 (by elcodedocle, 2014).
+ *
+ * Original written by Solar Designer <solar at openwall.com> in 2004-2006 and placed in
+ * the public domain.  Revised in subsequent years, still public domain.
+ *
+ * There's absolutely no warranty.
+ *
+ * The homepage URL for the original framework is:
+ *
+ *    http://www.openwall.com/phpass/
+ *
+ * Please be sure to update the Version line if you edit this file in any way.
+ * It is suggested that you leave the main version number intact, but indicate
+ * your project name (after the slash) and add your own revision information.
+ *
+ * Please do not change the "private" password hashing method implemented in
+ * here, thereby making your hashes incompatible.  However, if you must, please
+ * change the hash type identifier (the "$P$") to something different.
+ *
+ * Obviously, since this code is in the public domain, the above are not
+ * requirements (there can be none), but merely suggestions.
+ *
+ * elcodedocle's rev: I've ignored Solar Designer's recommendation but maintained full
+ * compatibility. Compatibility with future phpass versions is not warranted, though.
+ * 
+ * Latest version and revision notes available at https://github.com/elcodedocle/phpass
+ */
+
 class PasswordHash {
     var $itoa64;
     var $iteration_count_log2;
